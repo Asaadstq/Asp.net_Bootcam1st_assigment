9f765f37ec52e76f1ad2a9c3f737097c966e70fc
Asaadstq <asaadturkay@gmail.com>
2024-12-28 22:29:33 +0300

    1

---
+++ Script.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+//console.log("work")
+
+
+
+var phone= ['iphone','samsong','huawi']
+
+phone.forEach(element => {
+    if (element==='samsong'){
+        console.log("work")
+
+    }
+    else{
+        console.log("not work")
+    }
+    
+});
